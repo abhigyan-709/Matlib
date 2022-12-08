fe5fe3ea41631d9bd91121847b32b25cbaee9b2d
Abhigyan Kumar <abhigkumar709@gmail.com>
2022-12-08 19:39:39 +0530

    Final

--- matplot/views.py
+++ matplot/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+import matplotlib.pyplot as plt
+import io
+import urllib, base64
+
+
+def home(request):
+    plt.plot(range(10))
+    fig = plt.gcf()
+
+    buf = io.BytesIO()
+    fig.savefig(buf, format='png')
+    buf.seek(0)
+
+    string = base64.b64encode(buf.read())
+    uri = urllib.parse.quote(string)
+
+    return render(request, 'home.html', {'data':uri})
